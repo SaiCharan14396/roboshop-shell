bf37fbed26b2d9fa1d40fb0ff21feeb7c68f7ac4
sai charan <saicharanreddy14396@gmail.com>
2023-01-04 14:32:27 +0530

    declare variables

---
+++ mongodb.sh
@@ -0,0 +1,11 @@
+script_location=$(pwd)
+
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
+
+yum install mongodb-org -y
+
+systemctl enable mongod
+systemctl start mongod
+
+
+
