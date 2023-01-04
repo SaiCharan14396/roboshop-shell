15ac92cd6c4c56c1c47d76683093f53d573bcb36
sai charan <saicharanreddy14396@gmail.com>
2023-01-04 14:45:22 +0530

    mongo db install sed

--- mongodb.sh
+++ mongodb.sh
@@ -4,9 +4,11 @@ cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
 
 yum install mongodb-org -y
 
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
 #systemctl operations
 systemctl enable mongod
-systemctl start mongod
+systemctl restart mongod
 
 
 
