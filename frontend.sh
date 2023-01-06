6b57e128e2e9c2e4e233d1c42ba1e8a6df9b3b27
sai charan <saicharanreddy14396@gmail.com>
2023-01-06 13:12:04 +0530

    updating ./roboshop-shell

--- frontend.sh
+++ frontend.sh
@@ -11,7 +11,7 @@ fi
 
 echo -e "\e[35m remove nginx old content \e[0m"
 rm -rf /usr/share/nginx/html/* &>>${LOG}
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo success
 else
   echo failure
@@ -19,7 +19,7 @@ fi
 
 echo -e "\e[35m download frontend content \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo success
 else
   echo failure
@@ -29,7 +29,7 @@ cd /usr/share/nginx/html &>>${LOG}
 
 echo -e "\e[35m extract frontend content \e[0m"
 unzip /tmp/frontend.zip &>>${LOG}
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo success
 else
   echo failure
@@ -37,7 +37,7 @@ fi
 
 echo -e "\e[35m copy roboshop nginx config file \e[0m"
 cp  ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo success
 else
   echo failure
@@ -45,7 +45,7 @@ fi
 
 echo -e "\e[35m enable nginx \e[0m"
 systemctl enable nginx &>>${LOG}
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo success
 else
   echo failure
@@ -53,7 +53,7 @@ fi
 
 echo -e "\e[35m start nginx \e[0m"
 systemctl restart nginx &>>${LOG}
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo success
 else
   echo failure
