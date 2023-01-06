c17082baf29bb26ec8054c6ed4d88175385f7843
sai charan <saicharanreddy14396@gmail.com>
2023-01-06 15:08:13 +0530

    updating ./roboshop-shell

--- catalogue.sh
+++ catalogue.sh
@@ -3,7 +3,7 @@ LOG=/tmp/roboshop.log
 
 status_check() {
  if [ $? -eq 0 ]; then
-   echo success
+   echo -e "\e[32mSUCCESS\e[0m"
  else
    echo -e "\e[31mFAILURE\e[0m"
    echo "refer lof file for more info. LOG FILE PATH : ${LOG}"
