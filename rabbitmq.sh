337ef9d699fc4b5aa197aac788d564b43fd7a6bf
sai charan <saicharanreddy14396@gmail.com>
2023-01-09 17:08:25 +0530

    updating ./roboshop-shell

---
+++ rabbitmq.sh
@@ -0,0 +1,38 @@
+common source.sh
+
+if [ -z "${roboshop_rabbitmq_password}" ]; then
+  echo "variable roboshop_rabbitmq_password is needed"
+  exit
+fi
+
+print_head "configuring erlang YUM repos"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash &>>${LOG}
+status_check
+
+print_head "configuring rabbitmq YUM repos"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>${LOG}
+status_check
+
+print_head "install erlang and rabbitmq"
+yum install erlang rabbitmq-server -y &>>${LOG}
+status_check
+
+print_head "enable rabbitmq-server"
+systemctl enable rabbitmq-server &>>${LOG}
+status_check
+
+print_head "start rabbitmq-server"
+systemctl start rabbitmq-server &>>${LOG}
+status_check
+
+print_head "add rabbitmq user"
+rabbitmqctl add_user roboshop roboshop_rabbitmq_password &>>${LOG}
+status_check
+
+print_head "set tag for rabbitmq user"
+rabbitmqctl set_user_tags roboshop administrator &>>${LOG}
+status_check
+
+print_head "set permissions for rabbitmq user"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${LOG}
+status_check
