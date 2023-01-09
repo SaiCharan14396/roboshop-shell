a7fef8dc5af7015a95f51184c8b80d0538178325
sai charan <saicharanreddy14396@gmail.com>
2023-01-09 21:07:57 +0530

    updating ./roboshop-shell

--- payment.sh
+++ payment.sh
@@ -1,7 +1,7 @@
 source common.sh
 
 component=payment
-load_schema=false
+schema_load=false
 
 if [ -z "${roboshop_rabbitmq_password}" ]; then
   echo "variable roboshop_rabbitmq_password is needed"
