fcd599d60d4f14f49f1bc4ee369d8fe8a6e055f7
sai charan <saicharanreddy14396@gmail.com>
2023-01-09 17:25:04 +0530

    updating ./roboshop-shell

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -26,7 +26,10 @@ systemctl start rabbitmq-server &>>${LOG}
 status_check
 
 print_head "add rabbitmq user"
-rabbitmqctl add_user roboshop roboshop_rabbitmq_password &>>${LOG}
+rabbitmqctl list users | grep roboshop
+if [ $? -ne 0 ]; then
+  rabbitmqctl add_user roboshop roboshop_rabbitmq_password &>>${LOG}
+fi
 status_check
 
 print_head "set tag for rabbitmq user"
